581fc9ead4b9bf6d23ae6633d5ac49b681b86e4c
Gabriele Ghisleni <gabriele.ghisleni@spaziodati.eu>
2024-02-14 11:28:44 +0100

    add basic tests

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from unittest.mock import MagicMock, patch
+
+from elasticsearch import Elasticsearch, exceptions
+
+from src.llmescache.langchain import ElasticSearchCache
+
+
+@pytest.fixture
+def es_client_mock():
+    with patch.object(Elasticsearch, "__init__", return_value=None):
+        client_mock = MagicMock(spec=Elasticsearch)
+        client_mock.indices = MagicMock()
+        yield client_mock
+
+
+@pytest.fixture
+def cache(es_client_mock):
+    yield ElasticSearchCache(
+        es_client=es_client_mock,
+        es_index="test_index",
+        store_input=True,
+        store_timestamp=True,
+        store_input_params=True,
+        metadata={"test": "metadata"}
+    )
